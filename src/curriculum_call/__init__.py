bedfff64f015f4e46aea41060a16862ff144d207
musiye14 <1950655144@qq.com>
2022-04-12 13:15:27 +0800

    init问题

--- src/curriculum_call/__init__.py
+++ src/curriculum_call/__init__.py
@@ -2,7 +2,7 @@ import json
 
 
 def save(data):
-    with open("src/curriculum_call/class_data.json.json", "w") as f:
+    with open("src/curriculum_call/class_data.json", "w") as f:
         json.dump(data,f)
 class_data={'':[""]}
 save(class_data)
