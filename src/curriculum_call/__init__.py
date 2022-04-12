985d3fa2586f7f6cb77b429c7c88bc2dd09dbf1a
musiye14 <1950655144@qq.com>
2022-04-12 13:58:22 +0800

    对文件的存在判定

--- src/curriculum_call/__init__.py
+++ src/curriculum_call/__init__.py
@@ -1,8 +1,11 @@
 import json
-
+import os
+filename = 'src/curriculum_call/class_data.json'
 
 def save(data):
     with open("src/curriculum_call/class_data.json", "w") as f:
         json.dump(data,f)
-class_data={'':[""]}
-save(class_data)
+
+if not os.path.exists(filename):
+    class_data={'':[""]}
+    save(class_data)
