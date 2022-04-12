f7132af17b242cef1f0e595309e5cb8377dcac58
musiye14 <1950655144@qq.com>
2022-04-12 19:58:16 +0800

    自动同意好友\入群请求

--- src/autoagree/__init__.py
+++ src/autoagree/__init__.py
@@ -15,9 +15,6 @@ request_cmd = on_request(
 @request_cmd.handle()
 async def handle_request(bot: Bot, event: Union[GroupRequestEvent, FriendRequestEvent], state: T_State):
 
-    print("加群消息："+GroupRequestEvent.group_id)
-    print("好友消息:"+FriendRequestEvent.get_user_id)
-    print("是不是好友申请:"+isinstance(event,FriendRequestEvent))
     if isinstance(event, GroupRequestEvent):
         print("收到入群请求")
         await bot.set_group_add_request(
