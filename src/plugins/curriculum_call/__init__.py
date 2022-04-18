d50180d7c25e5e1c8ba0ac8abb6859a086842114
musiye14 <1950655144@qq.com>
2022-04-19 00:46:26 +0800

    查询明天课程调整

--- src/plugins/curriculum_call/__init__.py
+++ src/plugins/curriculum_call/__init__.py
@@ -421,5 +421,7 @@ async  def wacth(bot:Bot,event:Event):
                 elif (len(c) == 1):
                     if (week == int(c[0])):
                         msg = msg + day_time[transport_time] + "节:  " + course + "\n"
+    print(msg)
+    print(type(msg))
     await bot.send_private_msg(user_id=event.get_user_id(), message=msg)
     return
